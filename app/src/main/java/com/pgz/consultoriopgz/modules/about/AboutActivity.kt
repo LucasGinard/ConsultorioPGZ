8f981edb4f9f01769ea557567cca1995a3f66418
LucasGinard <contactolucasginard@gmail.com>
2023-05-16 22:03:37 -0400

    add button ,image and textview for about

--- app/src/main/java/com/pgz/consultoriopgz/modules/about/AboutActivity.kt
+++ app/src/main/java/com/pgz/consultoriopgz/modules/about/AboutActivity.kt
@@ -1,6 +1,7 @@
 package com.pgz.consultoriopgz.modules.about
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.pgz.consultoriopgz.R
 import com.pgz.consultoriopgz.databinding.ActivityAboutBinding
@@ -14,9 +15,20 @@ class AboutActivity: AppCompatActivity() {
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
         configureUI()
+        configureOnClickListeners()
     }
 
     private fun configureUI(){
         binding.header.tvTitleHeader.text = getText(R.string.title_about)
+    }
+
+    private fun configureOnClickListeners(){
+        binding.header.btnArrowBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnMoreInfo.setOnClickListener {
+            Toast.makeText(this,"",Toast.LENGTH_LONG).show()
+        }
     }
 }
